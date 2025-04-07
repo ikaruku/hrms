b1e8773c1a62ceb388ccf4434bfb8a6e2dd65357
unknown <ikaruku@gmail.com>
2025-04-07 14:24:30 +0700

    Update Home

--- resources/views/hr/attendance/home.blade.php
+++ resources/views/hr/attendance/home.blade.php
@@ -64,7 +64,7 @@
               <div class="icon">
                 <i class="ion ion-person"></i>
               </div>
-              <a href="" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{url('/hr/attendance/record')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           
